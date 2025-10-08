23208d672a1abe512a9870eda93b77c4313780d7
Abel Marnk <orinayobanji@gmail.com>
2025-10-08 11:33:46 +0100

    Initial commit

---
+++ week-0/ts/airdrop/convert.ts
@@ -0,0 +1,71 @@
+import prompt  from "prompt-sync";
+import bs58 from "bs58";
+
+const promptSync = prompt(); 
+
+const option = 
+    promptSync("What formart would you like to convert to(Supported (To-From)base-58|Json array): ").
+    toLowerCase();
+
+if (option.includes("base") && option.includes("58")){
+    const base58Key = promptSync("Enter the base 58 key:");
+    try {
+        const decoded = bs58.decode(base58Key);
+
+        const keyArray = Array.from(decoded);
+
+        console.log("\nJSON array:");
+        console.log(JSON.stringify(keyArray));
+
+    } catch (err) {
+        console.error("\nInvalid Base58 key.");
+    }
+
+} else if (option.includes("json") && option.includes("array")){
+    const jsonArray = promptSync("Enter the json array:");
+    try {
+        const parsedArray = JSON.parse(jsonArray);
+
+        if (!Array.isArray(parsedArray)) {
+            throw new Error("Invalid Json array key");
+        }
+
+        const uint8Array = Uint8Array.from(parsedArray);
+
+        const base58Key = bs58.encode(uint8Array);
+
+        console.log("\nBase 58 key:");
+        console.log(JSON.stringify(base58Key));
+
+    } catch (err) {
+        console.error("\nInvalid Json array key.");
+    }
+} else{
+    console.error("\nInvalid option.");
+}
+
+
+
+/*
+#[test]
+fn base58_to_wallet() {
+println!("Enter your name:");
+let stdin = io::stdin();
+let base58 = stdin.lock().lines().next().unwrap().unwrap(); //
+gdtKSTXYULQNx87fdD3YgXkzVeyFeqwtxHm6WdEb5a9YJRnHse7GQr7t5pbepsyvU Ck7Vv
+ksUGhPt4SZ8JHVSkt
+let wallet = bs58::decode(base58).into_vec().unwrap();
+println!("{:?}", wallet);
+}
+
+#[test]
+fn wallet_to_base58() {
+let wallet: Vec<u8> =
+vec![34,46,55,124,141,190,24,204,134,91,70,184,161,181,44,122,15,172,6
+3,62,153,150,99,255,202,89,105,77,41,89,253,130,27,195,134,14,66,75,24
+2,7,132,234,160,203,109,195,116,251,144,44,28,56,231,114,50,131,185,16
+8,138,61,35,98,78,53];
+let base58 = bs58::encode(wallet).into_string();
+println!("{:?}", base58);
+}
+*/
---
+++ week-0/ts/airdrop/enroll.ts
@@ -0,0 +1,136 @@
+import {
+address,
+appendTransactionMessageInstructions,
+assertIsTransactionWithinSizeLimit,
+createKeyPairSignerFromBytes,
+createSolanaRpc,
+createSolanaRpcSubscriptions,
+createTransactionMessage,
+devnet,
+getSignatureFromTransaction,
+pipe,
+sendAndConfirmTransactionFactory,
+setTransactionMessageFeePayerSigner,
+setTransactionMessageLifetimeUsingBlockhash,
+signTransactionMessageWithSigners,
+addSignersToTransactionMessage,
+getProgramDerivedAddress,
+generateKeyPairSigner,
+getAddressEncoder
+} from "@solana/kit";
+
+import { getInitializeInstruction, getSubmitTsInstruction } from "./clients/js/src/generated/index";
+
+import wallet from "./Turbin3-wallet.json";
+
+const MPL_CORE_PROGRAM = address("CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d");
+
+const PROGRAM_ADDRESS = address("TRBZyQHB3m68FGeVsqTK39Wm4xejadjVhP5MAZaKWDM");
+
+const SYSTEM_PROGRAM = address("11111111111111111111111111111111");
+
+// Create an rpc connection
+const rpc = createSolanaRpc(devnet("https://api.devnet.solana.com"));
+const rpcSubscriptions = createSolanaRpcSubscriptions(devnet('ws://api.devnet.solana.com'));
+
+const addressEncoder = getAddressEncoder();
+
+const COLLECTION = address("5ebsp5RChCGK7ssRZMVMufgVZhd2kFbNaotcZ5UvytN2");
+
+const keypair = await createKeyPairSignerFromBytes(new Uint8Array(wallet));
+
+// Create the PDA for enrollment account
+const accountSeeds = [Buffer.from("prereqs"), addressEncoder.encode(keypair.address)];
+
+const [account, _bump] = await getProgramDerivedAddress({
+    programAddress: PROGRAM_ADDRESS,
+    seeds: accountSeeds
+});
+
+// Create the PDA for the collection authority
+const collectionAuthoritySeeds = [Buffer.from("collection"), addressEncoder.encode(COLLECTION)];
+
+const [collectionAuthority, collectionAuthorityBump] = await getProgramDerivedAddress({
+    programAddress: PROGRAM_ADDRESS,
+    seeds: collectionAuthoritySeeds
+});
+
+// Generate mint keypair for the NFT
+const mintKeyPair = await generateKeyPairSigner();
+
+// Execute the initialize transaction
+const initializeIx = getInitializeInstruction({
+    github: "abelmarnk",
+    user: keypair,
+    account,
+    systemProgram: SYSTEM_PROGRAM
+});
+
+// Fetch latest blockhash
+const { value: latestBlockhash } = await rpc.getLatestBlockhash().send();
+
+const sendAndConfirmTransaction = sendAndConfirmTransactionFactory({rpc, rpcSubscriptions});
+
+
+/*
+const transactionMessageInit = pipe(
+    createTransactionMessage({ version: 0 }),
+    tx => setTransactionMessageFeePayerSigner(keypair, tx),
+    tx => setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, tx),
+    tx => appendTransactionMessageInstructions([initializeIx], tx)
+);
+
+const signedTxInit = await signTransactionMessageWithSigners(transactionMessageInit);
+
+assertIsTransactionWithinSizeLimit(signedTxInit);
+
+
+try {
+    const result = await sendAndConfirmTransaction(
+        signedTxInit,
+        { commitment: 'confirmed', skipPreflight: false }
+    );
+    console.log(result);
+    const signatureInit = getSignatureFromTransaction(signedTxInit);
+    console.log(`Success! Check out your TX here: https://explorer.solana.com/tx/${signatureInit}?cluster=devnet`);
+} catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+}
+*/
+
+
+// Execute the submitTs transaction
+const submitIx = getSubmitTsInstruction({
+    user: keypair,
+    account,
+    mint: mintKeyPair,
+    collection: COLLECTION,
+    authority:collectionAuthority,
+    mplCoreProgram: MPL_CORE_PROGRAM,
+    systemProgram: SYSTEM_PROGRAM
+});
+
+const transactionMessageSubmit = pipe(
+    createTransactionMessage({ version: 0 }),
+    tx => setTransactionMessageFeePayerSigner(keypair, tx),
+    tx => setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, tx),
+    tx => appendTransactionMessageInstructions([submitIx], tx),
+    tx => addSignersToTransactionMessage([mintKeyPair], tx)
+);
+
+const signedTxSubmit = await signTransactionMessageWithSigners(transactionMessageSubmit);
+
+assertIsTransactionWithinSizeLimit(signedTxSubmit);
+
+try {
+    await sendAndConfirmTransaction(
+        signedTxSubmit,
+        { commitment: 'confirmed', skipPreflight: false }
+    );
+    const signatureSubmit = getSignatureFromTransaction(signedTxSubmit);
+
+    console.log(`Success! Check out your TX here: https://explorer.solana.com/tx/${signatureSubmit}?cluster=devnet`);
+
+} catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+}
